c5e86655c137943da94dd97d7f2e9245880eb73c
Llewellyn Falco <isidore@setgame.com>
2018-05-22 10:33:47 -0400

    r removed yellow

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -34,23 +34,17 @@ class GildedRose {
 		}
 		if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
 
-			if (item.sellIn < 0 && item.quality > 0) {
-
-			}
+			// do nothing
 		} else {
 
-			if (item.quality > 0 && !item.name.equals("Sulfuras, Hand of Ragnaros")) {
+			if (item.quality > 0) {
 				item.quality = item.quality - 1;
 			}
 
-			if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-				item.sellIn = item.sellIn - 1;
-			}
+			item.sellIn = item.sellIn - 1;
 
 			if (item.sellIn < 0 && item.quality > 0) {
-				if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-					item.quality = item.quality - 1;
-				}
+				item.quality = item.quality - 1;
 			}
 		}
 	}
