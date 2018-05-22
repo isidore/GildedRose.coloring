6be509c1f192abd3c428d25dba4150e0e487f856
Llewellyn Falco <isidore@setgame.com>
2018-05-22 10:30:27 -0400

    r extract method

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -29,40 +29,48 @@ class GildedRose {
 			return;
 		}
 		if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-			if (item.quality < 50) {
-				item.quality = item.quality + 1;
-
-				if (item.sellIn < 11 && item.quality < 50) {
-					item.quality = item.quality + 1;
-				}
-
-				if (item.sellIn < 6 && item.quality < 50) {
-					item.quality = item.quality + 1;
-				}
-
-			}
-
-			item.sellIn = item.sellIn - 1;
-
-			if (item.sellIn < 0) {
-				item.quality = item.quality - item.quality;
-			}
+			updateQualityForBackstagePasses(item);
 
 		} else {
-			if (item.quality > 0 && !item.name.equals("Sulfuras, Hand of Ragnaros")) {
+			updateEverythingElse(item);
+
+		}
+	}
+
+	public void updateEverythingElse(Item item) {
+		if (item.quality > 0 && !item.name.equals("Sulfuras, Hand of Ragnaros")) {
+			item.quality = item.quality - 1;
+		}
+
+		if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+			item.sellIn = item.sellIn - 1;
+		}
+
+		if (item.sellIn < 0 && item.quality > 0) {
+			if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
 				item.quality = item.quality - 1;
 			}
+		}
+	}
 
-			if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-				item.sellIn = item.sellIn - 1;
+	public void updateQualityForBackstagePasses(Item item) {
+		if (item.quality < 50) {
+			item.quality = item.quality + 1;
+
+			if (item.sellIn < 11 && item.quality < 50) {
+				item.quality = item.quality + 1;
 			}
 
-			if (item.sellIn < 0 && item.quality > 0) {
-				if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-					item.quality = item.quality - 1;
-				}
+			if (item.sellIn < 6 && item.quality < 50) {
+				item.quality = item.quality + 1;
 			}
 
+		}
+
+		item.sellIn = item.sellIn - 1;
+
+		if (item.sellIn < 0) {
+			item.quality = item.quality - item.quality;
 		}
 	}
 
