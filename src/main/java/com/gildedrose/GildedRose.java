5045632aeb0146fc99b7c486a2e858451d8ea11a
Llewellyn Falco <isidore@setgame.com>
2018-05-22 10:28:08 -0400

    r removed red

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -30,37 +30,29 @@ class GildedRose {
 		}
 		if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
 			if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-				if (item.quality > 0) {
-					if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-						item.quality = item.quality - 1;
-					}
-				}
+
 			} else {
 				if (item.quality < 50) {
 					item.quality = item.quality + 1;
-			
+
 					if (item.sellIn < 11 && item.quality < 50) {
 						item.quality = item.quality + 1;
 					}
-			
+
 					if (item.sellIn < 6 && item.quality < 50) {
 						item.quality = item.quality + 1;
 					}
-			
+
 				}
 			}
-			
+
 			if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
 				item.sellIn = item.sellIn - 1;
 			}
-			
+
 			if (item.sellIn < 0) {
 				if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-					if (item.quality > 0) {
-						if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-							item.quality = item.quality - 1;
-						}
-					}
+
 				} else {
 					item.quality = item.quality - item.quality;
 				}
@@ -74,24 +66,12 @@ class GildedRose {
 					}
 				}
 			} else {
-				if (item.quality < 50) {
-					item.quality = item.quality + 1;
-			
-					if (item.sellIn < 11 && item.quality < 50) {
-						item.quality = item.quality + 1;
-					}
-			
-					if (item.sellIn < 6 && item.quality < 50) {
-						item.quality = item.quality + 1;
-					}
-			
-				}
 			}
-			
+
 			if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
 				item.sellIn = item.sellIn - 1;
 			}
-			
+
 			if (item.sellIn < 0) {
 				if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
 					if (item.quality > 0) {
@@ -100,7 +80,6 @@ class GildedRose {
 						}
 					}
 				} else {
-					item.quality = item.quality - item.quality;
 				}
 			}
 
