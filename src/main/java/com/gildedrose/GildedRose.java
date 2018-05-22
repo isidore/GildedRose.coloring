38feccb7f53edd791280de1cdf12734eaf1251d1
Llewellyn Falco <isidore@setgame.com>
2018-05-22 10:29:53 -0400

    r join and remove empty

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -29,47 +29,38 @@ class GildedRose {
 			return;
 		}
 		if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-			{
-				if (item.quality < 50) {
+			if (item.quality < 50) {
+				item.quality = item.quality + 1;
+
+				if (item.sellIn < 11 && item.quality < 50) {
 					item.quality = item.quality + 1;
-
-					if (item.sellIn < 11 && item.quality < 50) {
-						item.quality = item.quality + 1;
-					}
-
-					if (item.sellIn < 6 && item.quality < 50) {
-						item.quality = item.quality + 1;
-					}
-
 				}
+
+				if (item.sellIn < 6 && item.quality < 50) {
+					item.quality = item.quality + 1;
+				}
+
 			}
 
 			item.sellIn = item.sellIn - 1;
 
 			if (item.sellIn < 0) {
-				{
-					item.quality = item.quality - item.quality;
-				}
+				item.quality = item.quality - item.quality;
 			}
 
 		} else {
-			if (item.quality > 0) {
-				if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-					item.quality = item.quality - 1;
-				}
+			if (item.quality > 0 && !item.name.equals("Sulfuras, Hand of Ragnaros")) {
+				item.quality = item.quality - 1;
 			}
 
 			if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
 				item.sellIn = item.sellIn - 1;
 			}
 
-			if (item.sellIn < 0) {
-				if (item.quality > 0) {
-					if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-						item.quality = item.quality - 1;
-					}
+			if (item.sellIn < 0 && item.quality > 0) {
+				if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+					item.quality = item.quality - 1;
 				}
-
 			}
 
 		}
