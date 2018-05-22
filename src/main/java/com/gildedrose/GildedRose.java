a08b285eea3b030ca04f9319f8997a895d6eaabb
Llewellyn Falco <isidore@setgame.com>
2018-05-22 10:35:22 -0400

    r extract method

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -34,21 +34,21 @@ class GildedRose {
 		}
 		if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
 
-			// do nothing
 			updateQualityForSulfuras(item);
 			return;
 		}
-		{
+		updateDefaultItem(item);
+	}
 
-			if (item.quality > 0) {
-				item.quality = item.quality - 1;
-			}
+	public void updateDefaultItem(Item item) {
+		if (item.quality > 0) {
+			item.quality = item.quality - 1;
+		}
 
-			item.sellIn = item.sellIn - 1;
+		item.sellIn = item.sellIn - 1;
 
-			if (item.sellIn < 0 && item.quality > 0) {
-				item.quality = item.quality - 1;
-			}
+		if (item.sellIn < 0 && item.quality > 0) {
+			item.quality = item.quality - 1;
 		}
 	}
 
