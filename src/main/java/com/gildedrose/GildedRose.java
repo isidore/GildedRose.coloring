6b7a5178c646e61e9b1758fc5cfe777555ceeec5
Llewellyn Falco <isidore@setgame.com>
2018-05-22 10:24:29 -0400

    r automated cleanups (join and remove empty)

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -25,24 +25,18 @@ class GildedRose {
 	public void updateQualityForItem(Item item) {
 		if (item.name.equals("Aged Brie")) {
 
-			{
-				if (item.quality < 50) {
-					item.quality = item.quality + 1;
+			if (item.quality < 50) {
+				item.quality = item.quality + 1;
 
-					if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+				if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
 
-					}
 				}
 			}
 
 			item.sellIn = item.sellIn - 1;
 
-			if (item.sellIn < 0) {
-				{
-					if (item.quality < 50) {
-						item.quality = item.quality + 1;
-					}
-				}
+			if (item.sellIn < 0 && item.quality < 50) {
+				item.quality = item.quality + 1;
 			}
 		} else {
 
@@ -56,16 +50,12 @@ class GildedRose {
 				if (item.quality < 50) {
 					item.quality = item.quality + 1;
 
-					if (item.sellIn < 11) {
-						if (item.quality < 50) {
-							item.quality = item.quality + 1;
-						}
+					if (item.sellIn < 11 && item.quality < 50) {
+						item.quality = item.quality + 1;
 					}
 
-					if (item.sellIn < 6) {
-						if (item.quality < 50) {
-							item.quality = item.quality + 1;
-						}
+					if (item.sellIn < 6 && item.quality < 50) {
+						item.quality = item.quality + 1;
 					}
 
 				}
