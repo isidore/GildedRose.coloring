803757429d5d0af4984e5e0e429072b6f122c84b
Llewellyn Falco <isidore@setgame.com>
2018-05-22 10:28:58 -0400

    r removed yellow

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -29,9 +29,7 @@ class GildedRose {
 			return;
 		}
 		if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-			if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-
-			} else {
+			{
 				if (item.quality < 50) {
 					item.quality = item.quality + 1;
 
@@ -46,26 +44,19 @@ class GildedRose {
 				}
 			}
 
-			if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-				item.sellIn = item.sellIn - 1;
-			}
+			item.sellIn = item.sellIn - 1;
 
 			if (item.sellIn < 0) {
-				if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-
-				} else {
+				{
 					item.quality = item.quality - item.quality;
 				}
 			}
 
 		} else {
-			if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-				if (item.quality > 0) {
-					if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-						item.quality = item.quality - 1;
-					}
+			if (item.quality > 0) {
+				if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+					item.quality = item.quality - 1;
 				}
-			} else {
 			}
 
 			if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
@@ -73,14 +64,12 @@ class GildedRose {
 			}
 
 			if (item.sellIn < 0) {
-				if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-					if (item.quality > 0) {
-						if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-							item.quality = item.quality - 1;
-						}
+				if (item.quality > 0) {
+					if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+						item.quality = item.quality - 1;
 					}
-				} else {
 				}
+
 			}
 
 		}
