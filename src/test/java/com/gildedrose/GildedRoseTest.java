8a4181e5d0c786f22231d961e838301d59bfd55a
Llewellyn Falco <isidore@setgame.com>
2018-05-22 08:43:32 -0400

    Ready to go

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -1,7 +1,5 @@
 package com.gildedrose;
 
-import org.approvaltests.combinations.CombinationApprovals;
-import org.approvaltests.legacycode.Range;
 import org.approvaltests.reporters.UseReporter;
 import org.approvaltests.reporters.macosx.DiffMergeReporter;
 import org.junit.Test;
@@ -11,18 +9,15 @@ public class GildedRoseTest {
 
 	@Test
 	public void foo() throws Exception {
-		String names[] = { "name", "Aged Brie", "Backstage passes to a TAFKAL80ETC concert",
-				"Sulfuras, Hand of Ragnaros", };
-		Integer[] qualites = { 0, -1, 1, 49, 50, 51 };
-		Integer[] sellIn = Range.get(-1, 15);
-		CombinationApprovals.verifyAllCombinations(this::doStuff, names, qualites, sellIn);
+
 	}
 
-	public GildedRose doStuff(String name, Integer quality, Integer sellIn) {
+	public String doStuff(String name, Integer quality, Integer sellIn) {
 		Item[] items = { new Item(name, sellIn, quality) };
 		GildedRose gildedRose = new GildedRose(items);
 		gildedRose.updateQuality();
-		return gildedRose;
+		// Δ
+		return String.format("%s", gildedRose);
 	}
 
 }
