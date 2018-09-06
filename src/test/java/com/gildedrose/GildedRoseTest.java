7a1da3a7270dcbca140f1c9ef1afdf3a5a70c8db
Llewellyn Falco <isidore@setgame.com>
2018-09-06 19:24:12 +0200

    r extract local variable

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -11,7 +11,8 @@ public class GildedRoseTest {
         Item[] items = new Item[] { new Item("foo", 0, 0) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        assertEquals("foo", app.items[0].name);
+        String result = app.items[0].name;
+		assertEquals("foo", result);
     }
 
 }
