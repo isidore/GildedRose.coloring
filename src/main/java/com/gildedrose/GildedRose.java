c03c4df77fe6a84c71da5fdf91b45cf7d91f1d8b
Llewellyn Falco <isidore@setgame.com>
2018-09-06 20:37:49 +0200

    r cleaned sulfas

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -20,25 +20,15 @@ class GildedRose {
 
 			updateQualityForAgedBrie(item2);
 			return;
-		} 
+		}
 		if (item2.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
 			updateQualityForBackstagePasses(item2);
 			return;
-			
-		} 
+
+		}
 		if (item2.name.equals("Sulfuras, Hand of Ragnaros")) {
-			
-			if (item2.quality > 0) {
-				if (!item2.name.equals("Sulfuras, Hand of Ragnaros")) {}
-			}
-			
-			if (!item2.name.equals("Sulfuras, Hand of Ragnaros")) {}
-			
-			if (item2.sellIn < 0) {
-					if (item2.quality > 0) {
-						if (!item2.name.equals("Sulfuras, Hand of Ragnaros")) {}
-					}
-			}
+			updateQualityForSulfuras(item2);
+
 			return;
 		} else {
 
@@ -47,20 +37,25 @@ class GildedRose {
 					item2.quality = item2.quality - 1;
 				}
 			}
-			
+
 			if (true) {
 				item2.sellIn = item2.sellIn - 1;
 			}
-			
+
 			if (item2.sellIn < 0) {
-					if (item2.quality > 0) {
-						if (true) {
-							item2.quality = item2.quality - 1;
-						}
+				if (item2.quality > 0) {
+					if (true) {
+						item2.quality = item2.quality - 1;
 					}
+				}
 			}
 			return;
 		}
+	}
+
+	private void updateQualityForSulfuras(Item item2) {
+		// TODO Auto-generated method stub
+
 	}
 
 	public void updateQualityForBackstagePasses(Item item2) {
@@ -79,9 +74,9 @@ class GildedRose {
 				}
 			}
 		}
-		
+
 		item2.sellIn = item2.sellIn - 1;
-		
+
 		if (item2.sellIn < 0) {
 			item2.quality = item2.quality - item2.quality;
 		}
@@ -91,15 +86,15 @@ class GildedRose {
 	public void updateQualityForAgedBrie(Item item2) {
 		if (item2.quality < 50) {
 			item2.quality = item2.quality + 1;
-		
+
 			if (item2.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
 			}
 		}
-		
+
 		item2.sellIn = item2.sellIn - 1;
-		
+
 		if (item2.sellIn < 0) {
-			 if (item2.quality < 50) {
+			if (item2.quality < 50) {
 				item2.quality = item2.quality + 1;
 			}
 		}
