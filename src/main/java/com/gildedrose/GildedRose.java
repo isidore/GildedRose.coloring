dfd3f20e0f85d745358b9448c2c6cd122cf12ea8
Llewellyn Falco <isidore@setgame.com>
2018-09-06 20:06:30 +0200

    r cleaned if

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -27,10 +27,8 @@ class GildedRose {
 			items[i].sellIn = items[i].sellIn - 1;
 
 			if (items[i].sellIn < 0) {
-				 {
-					if (items[i].quality < 50) {
-						items[i].quality = items[i].quality + 1;
-					}
+				 if (items[i].quality < 50) {
+					items[i].quality = items[i].quality + 1;
 				}
 			}
 		} else {
