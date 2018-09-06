ebf74045cb9167af0cbe14a8a7f0f3e2f0f8f19d
Llewellyn Falco <isidore@setgame.com>
2018-09-06 19:46:09 +0200

    t added sellins

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -12,11 +12,12 @@ public class GildedRoseTest {
     public void foo() throws Exception {
         String[] names = {"foo","Aged Brie","Backstage passes to a TAFKAL80ETC concert", "Sulfuras, Hand of Ragnaros"};
         Integer[] qualities = {-1,0,1, 49,50,51};
-		CombinationApprovals.verifyAllCombinations(this::doStuff, names, qualities);
+        Integer[] sellIns = {0};
+		CombinationApprovals.verifyAllCombinations(this::doStuff, names, qualities, sellIns);
     }
 
-	public String doStuff(String name, Integer quality) {
-		Item[] items = new Item[] { new Item(name, 0, quality) };
+	public String doStuff(String name, Integer quality, Integer sellIn) {
+		Item[] items = new Item[] { new Item(name, sellIn, quality) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
         String result ="" +app.items[0];
