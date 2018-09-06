80346b2e3cbd3f968f110c1a564e8fd938924787
Llewellyn Falco <isidore@setgame.com>
2018-09-06 20:18:45 +0200

    r inline

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -17,7 +17,20 @@ class GildedRose {
 	public void updateQuality(int i, Item item) {
 		if (item.name.equals("Aged Brie")) {
 
-			updateQualityForAgedBrie(i);
+			if (items[i].quality < 50) {
+				items[i].quality = items[i].quality + 1;
+			
+				if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+				}
+			}
+			
+			items[i].sellIn = items[i].sellIn - 1;
+			
+			if (items[i].sellIn < 0) {
+				 if (items[i].quality < 50) {
+					items[i].quality = items[i].quality + 1;
+				}
+			}
 			return;
 		} 
 		if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
@@ -104,22 +117,6 @@ class GildedRose {
 				}
 			}
 			return;
-		}
-	}
-	public void updateQualityForAgedBrie(int i) {
-		if (items[i].quality < 50) {
-			items[i].quality = items[i].quality + 1;
-
-			if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-			}
-		}
-
-		items[i].sellIn = items[i].sellIn - 1;
-
-		if (items[i].sellIn < 0) {
-			 if (items[i].quality < 50) {
-				items[i].quality = items[i].quality + 1;
-			}
 		}
 	}
 }
