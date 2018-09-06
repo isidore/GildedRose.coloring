590ddd5fb7d53daf7cf0a17ad11f1fa307c6e149
Llewellyn Falco <isidore@setgame.com>
2018-09-06 19:47:45 +0200

    t 336 tests

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -13,7 +13,7 @@ public class GildedRoseTest {
     public void foo() throws Exception {
         String[] names = {"foo","Aged Brie","Backstage passes to a TAFKAL80ETC concert", "Sulfuras, Hand of Ragnaros"};
         Integer[] qualities = {-1,0,1, 49,50,51};
-        Integer[] sellIns = Range.get(0, 12);
+        Integer[] sellIns = Range.get(-1, 12	);
 		CombinationApprovals.verifyAllCombinations(this::doStuff, names, qualities, sellIns);
     }
 
