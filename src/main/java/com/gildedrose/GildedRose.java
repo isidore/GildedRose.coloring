1e25dad59833374193d3d0dd8d3bd6c2a834ee7a
Llewellyn Falco <isidore@setgame.com>
2018-09-06 20:12:17 +0200

    r extract method

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -17,20 +17,7 @@ class GildedRose {
 	public void foo(int i, Item item) {
 		if (item.name.equals("Aged Brie")) {
 
-			if (items[i].quality < 50) {
-				items[i].quality = items[i].quality + 1;
-
-				if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-				}
-			}
-
-			items[i].sellIn = items[i].sellIn - 1;
-
-			if (items[i].sellIn < 0) {
-				 if (items[i].quality < 50) {
-					items[i].quality = items[i].quality + 1;
-				}
-			}
+			doFooForAgedBrie(i);
 		} else {
 
 			if ( !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
@@ -71,6 +58,23 @@ class GildedRose {
 				} else {
 					items[i].quality = items[i].quality - items[i].quality;
 				}
+			}
+		}
+	}
+
+	public void doFooForAgedBrie(int i) {
+		if (items[i].quality < 50) {
+			items[i].quality = items[i].quality + 1;
+
+			if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+			}
+		}
+
+		items[i].sellIn = items[i].sellIn - 1;
+
+		if (items[i].sellIn < 0) {
+			 if (items[i].quality < 50) {
+				items[i].quality = items[i].quality + 1;
 			}
 		}
 	}
